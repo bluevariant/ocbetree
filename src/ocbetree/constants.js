1b08f2ae49101501046ca6d64dad40ca95f460da
Dong <0x317374@gmail.com>
2021-07-09 01:05:04 +0700

    chore: config

--- src/ocbetree/constants.js
+++ src/ocbetree/constants.js
@@ -5,4 +5,6 @@ const GITHUB_TAB_ATTR = "data-ocbetree-tab";
 OcbetreeConstants.GITHUB = {
   TAB_ATTR: GITHUB_TAB_ATTR,
   BLOB_CONTAINER: `#js-repo-pjax-container, div[itemtype="http://schema.org/SoftwareSourceCode"] main:not([${GITHUB_TAB_ATTR}]), [data-pjax-container]`,
+  INITIAL_SCROLL_TOP: 386,
+  TABS_HEIGHT: 50,
 };
